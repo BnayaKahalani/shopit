b7f80046050661b1897f143619b118fa5be4dd61
Bnaya Kahalani <bnaya50@gmail.com>
2023-05-13 21:58:25 +0300

    add navbar items - cart & user

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,8 @@ import logo from "../public/assets/images/logo.png"
 import Image from "next/image"
 import React from "react"
 import { IoSearchOutline } from "react-icons/io5"
+import { AiOutlineHeart, AiOutlineUser } from "react-icons/ai"
+import { BsCart2 } from "react-icons/bs"
 
 const Navbar = () => {
   return (
@@ -39,6 +41,27 @@ const Navbar = () => {
           />
           <span className='absolute w-8 h-8 rounded-full flex items-center justify-center top-1 right-1 bg-purple-500 txt-black text-xl'>
             <IoSearchOutline />
+          </span>
+        </div>
+        <div className='navbarHover'>
+          <AiOutlineHeart />
+          <div>
+            <p className='text-xs'>Recorder</p>
+            <h2 className='text-base font-semibold -mt-1'>My Items</h2>
+          </div>
+        </div>
+        <div className='navbarHover'>
+          <AiOutlineUser className='text-lg' />
+          <div>
+            <p className='text-xs'>Sign in</p>
+            <h2 className='text-base font-semibold -mt-1'>Account</h2>
+          </div>
+        </div>
+        <div className='flex flex-col justify-center items-center gap-2 h-12 px-5 rounded-full bg-transparent hover:bg-amber-400 duration-300 relative'>
+          <BsCart2 className='text-2xl' />
+          <p className='text-[10px]'>$0.00</p>
+          <span className='absolute w-4 h-4 bg-red-500 text-black top-0 right-4 rounded-full flex items-center justify-center font-bodyFont text-xs'>
+            0
           </span>
         </div>
       </div>
