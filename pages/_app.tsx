87e1768cb799ef4996acdd27ed0730a2ec5fb3d9
Bnaya Kahalani <bnaya50@gmail.com>
2023-06-03 21:20:44 +0300

    add Redux Toolkit store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,19 @@ import "slick-carousel/slick/slick-theme.css"
 import type { AppProps } from "next/app"
 import Layout from "@/components/Layout"
 import { Inter } from "next/font/google"
+import { Provider } from "react-redux"
+import { store } from "@/redux/store"
 
 const inter = Inter({ subsets: ["latin"], variable: "--inter-sans" })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${inter.variable} font-sans`}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </main>
+    <Provider store={store}>
+      <main className={`${inter.variable} font-sans`}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </main>
+    </Provider>
   )
 }
