f297897baa6a9c865251d68c6b53fc5982d44dc0
Bnaya Kahalani <bnaya50@gmail.com>
2023-07-24 11:38:40 +0300

    Searchbar functionality

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react"
 import { IoSearchOutline } from "react-icons/io5"
 import { Product } from "../type"
+import { useDispatch } from "react-redux"
 
 const SearchBar = () => {
+  const dispatch = useDispatch()
+
   const [productData, setProductData] = useState<Product[]>([])
   const [term, setTerm] = useState("")
   const [showOptions, setShowOptions] = useState(false)
@@ -26,53 +29,52 @@ const SearchBar = () => {
     if (term) setShowOptions(true)
     else setShowOptions(false)
   }, [term])
-  console.log("productData", productData)
 
   const handleClick = (option: any) => {
     console.log("You clicked:", option)
   }
 
   const renderedOptions = productData
-    .filter((option: any) => {
+    .filter((option) => {
       const regex = new RegExp(term, "i")
-      return regex.test(option.name)
+      return regex.test(option.title)
     })
     .map((option: any) => {
       return (
         <article
-          key={option.id}
+          key={option._id}
           onClick={() => handleClick(option)}
         >
-          {option.name}
+          {option.title.substring(0, 45) + "..."}
         </article>
       )
     })
 
+  const content = renderedOptions.length > 0 ? renderedOptions : "No matches found"
+
   return (
     <>
-      <div className='relative flex h-10 flex-1'>
-        <input
-          className='duration 200 h-full w-full rounded-full border-[1px] border-transparent px-4 text-base text-black outline-none focus-visible:border-black'
-          type='text'
-          placeholder='Search at Shopit'
-          value={term}
-          onChange={handleChange}
-        />
-        <span className='txt-black absolute right-1 top-1 flex h-8 w-8 items-center justify-center rounded-full bg-purple-500 text-xl'>
-          <IoSearchOutline />
-        </span>
+      <div className='relative flex h-10 flex-1 flex-col justify-center'>
+        <div className='flex flex-col gap-5'>
+          <input
+            className='h-[45px] w-full rounded-full border-[1px] border-transparent px-4 text-base text-black outline-none focus-visible:border-black'
+            type='text'
+            placeholder='Search at Shopit'
+            value={term}
+            onChange={handleChange}
+          />
+          <span className='txt-black absolute right-1 top-1 flex h-8 w-8 items-center justify-center rounded-full bg-purple-500 text-xl'>
+            <IoSearchOutline />
+          </span>
+        </div>
+        {showOptions && (
+          <div className=' absolute top-11 w-full rounded-3xl border-[1px] border-white bg-amber-500 p-3'>
+            {content}
+          </div>
+        )}
       </div>
-      {/* {showOptions && <div className='absolute'>{renderedOptions}</div>} */}
     </>
   )
 }
 
 export default SearchBar
-
-export const getServerSideProps = async () => {
-  const productData = await (await fetch("http://localhost:3000/api/productdata")).json()
-
-  return {
-    props: { productData },
-  }
-}
