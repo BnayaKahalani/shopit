bcaaf0cbb4f47eb2d8db4548ddb2cee61fc1d221
Bnaya Kahalani <bnaya50@gmail.com>
2023-08-16 14:36:23 +0300

    deploy 2.2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,8 @@ import { Inter } from "next/font/google"
 import { Provider } from "react-redux"
 import { store, persistor } from "@/redux/store"
 import { PersistGate } from "redux-persist/integration/react"
+import Image from "next/image"
+import loader from "../public/assets/images/loader.gif"
 // import { SessionProvider } from "next-auth/react"
 
 const inter = Inter({ subsets: ["latin"], variable: "--inter-sans" })
@@ -16,7 +18,16 @@ export default function App({ Component, pageProps: { ...pageProps } }: AppProps
     <Provider store={store}>
       {/* <SessionProvider session={session}> */}
       <PersistGate
-        loading={"loading"}
+        loading={
+          <div className='text-center'>
+            <Image
+              src={loader}
+              width={400}
+              height={400}
+              alt='Loading...'
+            />
+          </div>
+        }
         persistor={persistor}
       >
         <main className={`${inter.variable} font-sans`}>
